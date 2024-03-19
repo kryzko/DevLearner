71105a22e732f84220533ffaadb1cc606e7ee802
kryzko <66265498+kryzko@users.noreply.github.com>
2024-03-19 17:16:21 +0300

    Добавьте файлы проекта.

---
+++ Models/UsersInfo.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DevLearner.Models
+{
+    public class UsersInfo
+    {
+        public int Id { get; set; }
+
+        [BindProperty]
+        public int ProjectId { get; set; }
+
+        [BindProperty]
+        public string Name { get; set; }
+
+        UsersInfo() { }
+
+    }
+}
